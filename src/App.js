ae4a32765b7732b1cc6a66f09250e95d782a4532
dinabecirovic <dinaabecirovic@gmail.com>
2024-12-03 10:34:57 +0100

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="routes">
+      <Routes>
+        <Route path="/" element={<h1>Welcome!</h1>} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sidebar" element={<Sidebar/>} />
+      </Routes>
     </div>
   );
 }
